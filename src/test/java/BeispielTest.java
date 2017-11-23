a3a3abe44f7d0953c232820f858ca7e91b416cb4
Joachim Basler <joachim.basler@maibornwolff.de>
2017-11-23 11:52:04 +0100

    mini-bsp test um zu schauen ob es mit jenkins tut

--- src/test/java/BeispielTest.java
+++ src/test/java/BeispielTest.java
@@ -1,2 +1,17 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class BeispielTest {
+
+    @Test
+    public void testTest(){
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.de");
+        Assert.assertEquals(driver.getTitle(), "Google");
+
+        driver.close();
+    }
+
 }
